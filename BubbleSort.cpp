f7ae1186dded0497348fa7ec4d1fc9785917469c
Aditya Raj <91009983+adi1719@users.noreply.github.com>
2023-03-02 21:14:36 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -3,12 +3,18 @@ void bubbleSort(vector<int>& arr, int n)
 {   
     for(int i=1; i<n; i++)
     {
+        bool swapp = false;
         for(int j= 0; j<n-i; j++)
         {
             if(arr[j]>arr[j+1])
             {
                 swap(arr[j], arr[j+1]);
+                swapp =true;
             }
+        }
+        if(swapp == false)
+        {
+            break;
         }
     }
 }
